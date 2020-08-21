0f62cdef9f2c556232e40f05fe7b488b694e50db
AbdElhafizIslam <36646694+AbdElhafizIslam@users.noreply.github.com>
2020-08-21 11:36:58 +0200

    fixed an error in the server.js

--- server/server.js
+++ server/server.js
@@ -47,15 +47,14 @@ app.get('/todos/:id',(req,res)=>{
   Todo.findById(id).then((todo)=>{
     if (!todo)
     {
-      res.status(400).send();
+        return res.status(400).send();
     }
-    if (todo){
-      res.send({todo});
-    }
-    else (!todo)
-      res.status(404).send();
+
+    res.send({todo});
 
 
+  }).catch((e)=>{
+    res.status(400).send();
   });
 
 
